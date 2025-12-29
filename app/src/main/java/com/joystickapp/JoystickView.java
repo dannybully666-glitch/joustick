997fe853ff32197694e2307c1fb0bc4d35af7161
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 19:28:58 +0530

    Update JoystickView.java

--- app/src/main/java/com/joystickapp/JoystickView.java
+++ app/src/main/java/com/joystickapp/JoystickView.java
@@ -2,64 +2,37 @@ package com.joystickapp;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.MotionEvent;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class JoystickView extends View {
 
-    private float centerX, centerY;
-    private float baseRadius = 150f;
-    private float hatRadius = 60f;
-    private float hatX, hatY;
-
-    private Paint basePaint = new Paint();
-    private Paint hatPaint = new Paint();
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public JoystickView(Context context) {
         super(context);
-        basePaint.setColor(Color.GRAY);
-        basePaint.setAntiAlias(true);
-
-        hatPaint.setColor(Color.DKGRAY);
-        hatPaint.setAntiAlias(true);
+        init();
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        centerX = w / 2f;
-        centerY = h / 2f;
-        hatX = centerX;
-        hatY = centerY;
+    public JoystickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public JoystickView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint.setColor(0xFF444444);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(centerX, centerY, baseRadius, basePaint);
-        canvas.drawCircle(hatX, hatY, hatRadius, hatPaint);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float dx = event.getX() - centerX;
-        float dy = event.getY() - centerY;
-        double distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (event.getAction() != MotionEvent.ACTION_UP) {
-            if (distance < baseRadius) {
-                hatX = event.getX();
-                hatY = event.getY();
-            } else {
-                hatX = (float) (centerX + dx / distance * baseRadius);
-                hatY = (float) (centerY + dy / distance * baseRadius);
-            }
-        } else {
-            hatX = centerX;
-            hatY = centerY;
-        }
-
-        invalidate();
-        return true;
+        super.onDraw(canvas);
+        float r = Math.min(getWidth(), getHeight()) / 2f - 10;
+        canvas.drawCircle(getWidth()/2f, getHeight()/2f, r, paint);
     }
 }
