8623618a1feb610548e178c5d20fc3eb55fd24d7
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 20:29:03 +0530

    Update MainActivity.java

--- app/src/main/java/com/joystickapp/MainActivity.java
+++ app/src/main/java/com/joystickapp/MainActivity.java
@@ -1,23 +1,24 @@
 package com.joystickapp;
 
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GamepadHidService gamepad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gamepad = new GamepadHidService();
+        gamepad.init(this);
+
         JoystickView joystick = findViewById(R.id.joystick);
 
-        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
-            @Override
-            public void onMove(float x, float y) {
-                Log.d("JOYSTICK", "X=" + x + " Y=" + y);
-            }
+        joystick.setOnMoveListener((x, y) -> {
+            gamepad.sendAxis(x, y);
         });
     }
 }
