eb7f7361f6bbb5e0f9f7dc7daf680f3f0bfcf15e
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 22:48:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/joystickapp/MainActivity.java
+++ app/src/main/java/com/joystickapp/MainActivity.java
@@ -1,70 +1,19 @@
 package com.joystickapp;
 
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        status = findViewById(R.id.statusText);
     }
 
-    // Called by JoystickView
     public void onDirection(String dir) {
-        status.setText(dir);
-
-        WASDAccessibilityService svc = WASDAccessibilityService.instance;
-        if (svc == null) return;
-
-        // Release previous keys first
-        svc.releaseAll();
-
-        switch (dir) {
-            case "W":
-                svc.pressW();
-                break;
-            case "A":
-                svc.pressA();
-                break;
-            case "S":
-                svc.pressS();
-                break;
-            case "D":
-                svc.pressD();
-                break;
-            case "WA":
-                svc.pressW();
-                svc.pressA();
-                break;
-            case "WD":
-                svc.pressW();
-                svc.pressD();
-                break;
-            case "SA":
-                svc.pressS();
-                svc.pressA();
-                break;
-            case "SD":
-                svc.pressS();
-                svc.pressD();
-                break;
+        if (WASDAccessibilityService.instance != null) {
+            WASDAccessibilityService.instance.onDirection(dir);
         }
-    }
-
-    // Called when joystick is released (VERY IMPORTANT)
-    public void onRelease() {
-        WASDAccessibilityService svc = WASDAccessibilityService.instance;
-        if (svc != null) {
-            svc.releaseAll();
-        }
-        status.setText("CENTER");
     }
 }
