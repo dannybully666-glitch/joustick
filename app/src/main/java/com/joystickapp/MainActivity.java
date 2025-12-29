0534153790338a290b52d78a6bc449230b75bc42
dannybully666-glitch <dannybully666@gmail.com>
2025-12-30 00:19:16 +0530

    Update MainActivity.java

--- app/src/main/java/com/joystickapp/MainActivity.java
+++ app/src/main/java/com/joystickapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.joystickapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+        // Request overlay permission
+        if (!Settings.canDrawOverlays(this)) {
+            Intent i = new Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(i);
+        }
+
+        // Start overlay service
+        startService(new Intent(this, JoystickOverlayService.class));
+
+        finish(); // No UI needed
     }
 }
