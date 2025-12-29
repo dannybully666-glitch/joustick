97e7ddc0cfd32f7dec6f5f058b5ce593c7897e2b
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 20:58:50 +0530

    Update MainActivity.java

--- app/src/main/java/com/joystickapp/MainActivity.java
+++ app/src/main/java/com/joystickapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.joystickapp;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private boolean w, a, s, d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,46 +12,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         JoystickView joystick = findViewById(R.id.joystick);
+        TextView debugText = findViewById(R.id.debugText);
 
-        joystick.setOnMoveListener((angle, strength) -> {
-            if (strength < 0.2f) {
-                releaseAll();
-                return;
-            }
+        joystick.setOnMoveListener((x, y) -> {
+            StringBuilder out = new StringBuilder();
 
-            boolean nw = false, na = false, ns = false, nd = false;
+            if (y > 0.4f) out.append("W ");
+            if (y < -0.4f) out.append("S ");
+            if (x < -0.4f) out.append("A ");
+            if (x > 0.4f) out.append("D ");
 
-            if (angle >= 337.5 || angle < 22.5) nd = true;
-            else if (angle < 67.5) { nd = true; nw = true; }
-            else if (angle < 112.5) nw = true;
-            else if (angle < 157.5) { nw = true; na = true; }
-            else if (angle < 202.5) na = true;
-            else if (angle < 247.5) { na = true; ns = true; }
-            else if (angle < 292.5) ns = true;
-            else { ns = true; nd = true; }
-
-            updateKey("W", w, nw); w = nw;
-            updateKey("A", a, na); a = na;
-            updateKey("S", s, ns); s = ns;
-            updateKey("D", d, nd); d = nd;
-        });
-    }
-
-    private void updateKey(String key, boolean oldState, boolean newState) {
-        if (oldState != newState) {
-            if (newState) {
-                Log.d("WASD", key + " DOWN");
+            if (out.length() == 0) {
+                debugText.setText("CENTER");
             } else {
-                Log.d("WASD", key + " UP");
+                debugText.setText(out.toString().trim());
             }
-        }
-    }
-
-    private void releaseAll() {
-        updateKey("W", w, false);
-        updateKey("A", a, false);
-        updateKey("S", s, false);
-        updateKey("D", d, false);
-        w = a = s = d = false;
+        });
     }
 }
