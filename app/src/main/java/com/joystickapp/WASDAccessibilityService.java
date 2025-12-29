7f54a66d9ed9a503c2a6b43ca4f4410b838c2dec
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 23:11:50 +0530

    Update WASDAccessibilityService.java

--- app/src/main/java/com/joystickapp/WASDAccessibilityService.java
+++ app/src/main/java/com/joystickapp/WASDAccessibilityService.java
@@ -1,7 +1,7 @@
 package com.joystickapp;
 
 import android.accessibilityservice.AccessibilityService;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
 public class WASDAccessibilityService extends AccessibilityService {
@@ -12,20 +12,57 @@ public class WASDAccessibilityService extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
         instance = this;
-        Log.d("WASD", "Accessibility service connected");
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Not used
+        // Not needed
     }
 
     @Override
     public void onInterrupt() {
+        // Not needed
     }
 
-    // CALLED FROM ACTIVITY
-    public void onDirection(String dir) {
-        Log.d("WASD", "Direction: " + dir);
+    // ===== PUBLIC API USED BY MAINACTIVITY =====
+
+    public void press(int keyCode) {
+        sendKey(KeyEvent.ACTION_DOWN, keyCode);
+    }
+
+    public void releaseAll() {
+        int[] keys = {
+                KeyEvent.KEYCODE_W,
+                KeyEvent.KEYCODE_A,
+                KeyEvent.KEYCODE_S,
+                KeyEvent.KEYCODE_D
+        };
+
+        for (int key : keys) {
+            sendKey(KeyEvent.ACTION_UP, key);
+        }
+    }
+
+    // ===== INTERNAL SAFE KEY INJECTION =====
+
+    private void sendKey(int action, int keyCode) {
+        long now = System.currentTimeMillis();
+
+        KeyEvent event = new KeyEvent(
+                now,
+                now,
+                action,
+                keyCode,
+                0,
+                0,
+                KeyEvent.KEYCODE_UNKNOWN,
+                0,
+                KeyEvent.FLAG_FROM_SYSTEM,
+                KeyEvent.KEYCODE_UNKNOWN
+        );
+
+        // IMPORTANT: AccessibilityService does NOT have dispatchKeyEvent()
+        // We inject using this method instead:
+        super.onKeyEvent(event);
     }
 }
