376b780183651adc61a4850f757ff2e663d4785e
dannybully666-glitch <dannybully666@gmail.com>
2025-12-30 14:21:40 +0530

    Update MacroEngine.java

--- app/src/main/java/com/joystickapp/macro/MacroEngine.java
+++ app/src/main/java/com/joystickapp/macro/MacroEngine.java
@@ -8,12 +8,12 @@ public class MacroEngine {
 
     Handler h = new Handler();
 
-    public void run(int[] keys, int delayMs) {
+    public void run(int[] keys, int delay) {
         for (int i = 0; i < keys.length; i++) {
             int k = keys[i];
             h.postDelayed(() ->
                 InputAccessibilityService.instance.sendKey(k),
-                i * delayMs
+                i * delay
             );
         }
     }
