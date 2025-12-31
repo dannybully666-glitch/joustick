cab4a07e8e221a3a19fa8bce27b40c78dabcf6db
dannybully666-glitch <dannybully666@gmail.com>
2025-12-31 14:45:48 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/virtualcontainer/MainActivity.kt
+++ app/src/main/java/com/example/virtualcontainer/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.virtualcontainer
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TEMP: launch directly
-        val intent = Intent(this, GameContainerActivity::class.java)
-        intent.putExtra("TARGET_PACKAGE", "com.example.targetgame")
-        startActivity(intent)
-        finish()
+        setContentView(android.R.layout.simple_list_item_1)
     }
 }
