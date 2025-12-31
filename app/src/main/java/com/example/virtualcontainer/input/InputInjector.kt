95248eed2e969af5804ff3f93dcaecd3cb9885d1
dannybully666-glitch <dannybully666@gmail.com>
2025-12-31 13:52:20 +0530

    Update InputInjector.kt

--- app/src/main/java/com/example/virtualcontainer/input/InputInjector.kt
+++ app/src/main/java/com/example/virtualcontainer/input/InputInjector.kt
@@ -1,1 +1,9 @@
+package com.example.virtualcontainer.input
 
+import android.util.Log
+
+object InputInjector {
+    fun inject(x: Float, y: Float) {
+        Log.d("Injector", "Analog x=$x y=$y")
+    }
+}
