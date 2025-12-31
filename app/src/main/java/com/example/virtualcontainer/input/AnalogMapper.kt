8f367e904a182a48b6e2d4bca98fc8f0d8fdf3f2
dannybully666-glitch <dannybully666@gmail.com>
2025-12-31 13:52:03 +0530

    Update AnalogMapper.kt

--- app/src/main/java/com/example/virtualcontainer/input/AnalogMapper.kt
+++ app/src/main/java/com/example/virtualcontainer/input/AnalogMapper.kt
@@ -1,1 +1,13 @@
+package com.example.virtualcontainer.input
 
+object AnalogMapper {
+    fun toDirection(x: Float, y: Float): String {
+        return when {
+            y < -0.5 -> "UP"
+            y > 0.5 -> "DOWN"
+            x < -0.5 -> "LEFT"
+            x > 0.5 -> "RIGHT"
+            else -> "CENTER"
+        }
+    }
+}
