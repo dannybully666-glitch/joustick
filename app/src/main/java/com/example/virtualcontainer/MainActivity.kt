54f9c3a3e8e5c3e7f3d6c21dae931d26789cb217
dannybully666-glitch <dannybully666@gmail.com>
2025-12-31 15:23:16 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/virtualcontainer/MainActivity.kt
+++ app/src/main/java/com/example/virtualcontainer/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.virtualcontainer
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import com.example.virtualcontainer.overlay.OverlayService
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(android.R.layout.simple_list_item_1)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION
+                )
+                startActivity(intent)
+            } else {
+                startOverlay()
+            }
+        } else {
+            startOverlay()
+        }
+    }
+
+    private fun startOverlay() {
+        val intent = Intent(this, OverlayService::class.java)
+        startService(intent)
     }
 }
