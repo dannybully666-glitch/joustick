de88b9745a8cec237129615c35023657ae41213e
dannybully666-glitch <dannybully666@gmail.com>
2026-01-01 14:57:01 +0530

    Update GhostIME.java

--- app/src/main/java/com/example/ghostjoystick/GhostIME.java
+++ app/src/main/java/com/example/ghostjoystick/GhostIME.java
@@ -2,20 +2,34 @@ package com.example.ghostjoystick;
 
 import android.inputmethodservice.InputMethodService;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.view.LayoutInflater;
 
 public class GhostIME extends InputMethodService {
 
-    public static GhostIME INSTANCE;
-
     @Override
-    public void onCreate() {
-        super.onCreate();
-        INSTANCE = this;
+    public View onCreateInputView() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View keyboard = inflater.inflate(R.layout.ime_keyboard, null);
+
+        KeyboardView helper = new KeyboardView(this, this);
+        helper.bind(keyboard);
+
+        return keyboard;
     }
 
-    public static void sendKey(int key) {
-        if (INSTANCE == null) return;
+    void sendDown(int keyCode) {
+        InputConnection ic = getCurrentInputConnection();
+        if (ic == null) return;
 
-        INSTANCE.sendDownUpKeyEvents(key);
+        ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, keyCode));
+    }
+
+    void sendUp(int keyCode) {
+        InputConnection ic = getCurrentInputConnection();
+        if (ic == null) return;
+
+        ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, keyCode));
     }
 }
